0778143d2df67b6a036a6698d6bff663e2c86933
nour-aboalzin <138358669+nour-aboalzin@users.noreply.github.com>
2024-11-04 19:07:19 +0300

    ..

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->first_name(),
+            'last_name' => fake()->last_name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
